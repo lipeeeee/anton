f262b1ad77a9d836fe08ffe8dbcf2c3783e1e03f
lipeeeee <filipe.pereirafga@gmail.com>
2023-06-23 00:28:47 +0100

    documentation go brr

--- anton/league_connection.py
+++ anton/league_connection.py
@@ -9,13 +9,27 @@ class LeagueConnection:
 
     Attributes:
         base_url (str): the base url for the `LCA` interface
+        protocol (str): specification of http or https protocols
+        port (str): the port where `LCA` is being hosted on(riot version, normal version is 2999)
+        username (str): As of patch 13.12 the username used in auth is a constant value("riot")
+        remoting_auth_token (str): Auth key
+    
+    Example request:
+        GET https://127.0.0.1:2999/liveclientdata/allgamedata
     """
 
     base_url: str
+    protocol: str
+    port: str
+    username: str
+    remoting_auth_token: str
 
     def __init__(self) -> None:
-        """."""
-        base_url = ""
+        self.protocol = "https"
+        
+    def build_url(self, o: object) -> str:
+        """.""" 
+        return ""
 
     def get(self, url) -> object:
         """."""
